fb84e31ae86b78992816e97f57133b5ae29e9bbb
Nathan Peters <peters.nate8@gmail.com>
2024-07-05 15:15:26 -0600

    can search for breweries by name, redirect to results page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    render 'home'
   end
 
   def results
+    @data = {query: params{:query}}.to_json
+    render 'results'
   end
 
   def brewery
-    
+    render 'brewery'
   end
 end
--- app/javascript/src/home/home.jsx
+++ app/javascript/src/home/home.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '@src/layout';
-
+import { GetBreweriesByName } from '../utils/breweryDBRequests';
 import './home.scss';
 
 const Home = (props) => {
+
+  const [name, setName] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const encodedName = encodeURIComponent(name);
+    const underscoreName = name.replace(' ', '_');
+
+    // GetBreweriesByName(encodedName, (response) => {
+    //   console.log(response);
+    //   setResults(response);
+    // })
+
+    window.location.href = `/results/${underscoreName}`;
+  };
+
   return (
     <Layout>
       <div className="container pt-5">
         <div className="row">
           <h4 className='text-center'>Search for breweries using any of all fields below:</h4>
         </div>
-        <form className='row d-flex justify-content-center mt-4 border py-3'>
-          <div className="col-10 text-center">
+        <form className='row d-flex justify-content-center mt-4 border py-3' onSubmit={handleSearch}>
+          <div className="col-8 text-center">
             <label htmlFor="breweryName" className='form-label'>Brewery Name</label>
-            <input type="text" id="breweryName" name='name' className='form-control text-center'/>
+            <input type="search" id="breweryName" name='name' className='form-control text-center' onChange={(event) => setName(event.target.value)} value={name}/>
           </div>
           <div className="col-5 text-center mt-3">
             <label htmlFor="state" className='form-label'>State</label>
