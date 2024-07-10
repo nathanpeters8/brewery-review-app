3bd516eea8b3aa4120874c4d5af947c2459c56b4
Nathan Peters <peters.nate8@gmail.com>
2024-07-10 13:36:46 -0600

    add pagination for brewery results page

--- app/javascript/src/results/results.jsx
+++ app/javascript/src/results/results.jsx
@@ -2,33 +2,49 @@ import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { GetBreweries } from '@utils/breweryDBRequests';
+import { MapModalTemplate } from '@utils/modalTemplates';
 import Layout from '@utils/layout';
-import { Modal, Button } from 'react-bootstrap';
-
+import PaginationButtons from './paginationButtons';
 import './results.scss';
-import { MapModalTemplate } from '../utils/modalTemplates';
 
 const Results = ({ queryParams }) => {
+  // state variables
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [showMap, setShowMap] = useState(false);
   const [clickedBrewery, setClickedBrewery] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pagesArray, setPagesArray] = useState([1, 2, 3]);
+  
+  const itemsPerPage = 10;
 
+  // update query state when queryParams change
   useEffect(() => {
     const params = new URLSearchParams(queryParams);
     setQuery(JSON.parse(params.keys().next().value));
   }, [queryParams]);
 
+  // fetch breweries when query or currentPage changes
   useEffect(() => {
     if (query) {
-      console.log(query);
-      GetBreweries(query, (response) => {
+      GetBreweries(query, currentPage, itemsPerPage, (response) => {
         console.log(response);
         setResults(response);
       });
     }
-  }, [query]);
+  }, [query, currentPage]);
 
+  // update pagesArray when currentPage changes
+  useEffect(() => {
+    if(currentPage > 1) {
+      setPagesArray([currentPage-1, currentPage, currentPage+1]);
+    }
+    else if(currentPage === 1) {
+      setPagesArray([1, 2, 3]);
+    }
+  }, [currentPage]);
+
+  // show map modal when clickedBrewery changes
   useEffect(() => {
     if (clickedBrewery) {
       setShowMap(true);
@@ -37,17 +53,28 @@ const Results = ({ queryParams }) => {
     }
   }, [clickedBrewery]);
 
+  // handle brewery click
   const handleBreweryClick = (e, id) => {
     e.preventDefault();
     window.location.href = `/brewery/${id}`;
   };
 
+  // handle page change
+  const handlePageChange = (e, num) => {
+    setCurrentPage(num);
+    e.target.blur();
+  }
+
+  // format phone number
   const formatPhoneNumber = (num) => `(${num.slice(0, 3)}) ${num.slice(3, 6)}-${num.slice(6)}`;
 
   return (
     <Layout>
       <div className='container mt-4'>
         <h4 className='text-center'>Search Results</h4>
+        {results.length >= itemsPerPage && (
+          <PaginationButtons handlePageChange={handlePageChange} currentPage={currentPage} pagesArray={pagesArray} />
+        )}
         <div id='breweryResults' className='row mt-5'>
           {(() => {
             if (results.length == 0) {
@@ -95,13 +122,23 @@ const Results = ({ queryParams }) => {
             });
           })()}
         </div>
+        {results.length >= itemsPerPage && (
+          <PaginationButtons handlePageChange={handlePageChange} currentPage={currentPage} pagesArray={pagesArray} />
+        )}
       </div>
       {(() => {
         if (!clickedBrewery) {
           return null;
         }
         return (
-          <MapModalTemplate showMap={showMap} toggleShowMap={setShowMap} name={clickedBrewery.name} city={clickedBrewery.city} state={clickedBrewery.state} street={clickedBrewery.street}/>
+          <MapModalTemplate
+            showMap={showMap}
+            toggleShowMap={setShowMap}
+            name={clickedBrewery.name}
+            city={clickedBrewery.city}
+            state={clickedBrewery.state}
+            street={clickedBrewery.street}
+          />
         );
       })()}
     </Layout>
--- app/javascript/src/utils/breweryDBRequests.js
+++ app/javascript/src/utils/breweryDBRequests.js
@@ -1,15 +1,15 @@
 import { safeCredentials, safeCredentialsFormData, handleErrors } from './fetchHelper';
 
-export const GetBreweries = (query, callback) => {
+export const GetBreweries = (query, page, per_page, callback) => {
   let params = '';
   if (query.hasOwnProperty('name')) params += `by_name=${query.name}&`;
   if (query.hasOwnProperty('city')) params += `by_city=${query.city}&`;
   if (query.hasOwnProperty('state')) params += `by_state=${query.state}&`;
   if (query.hasOwnProperty('postal')) params += `by_postal=${query.postal}&`;
 
-  if(params.endsWith("&")) params = params.slice(0, -1);
+  if(params.endsWith("&") || params.endsWith('#')) params = params.slice(0, -1);
 
-  fetch(`https://api.openbrewerydb.org/v1/breweries?${params}`)
+  fetch(`https://api.openbrewerydb.org/v1/breweries?${params}&page=${page}&per_page=${per_page}`)
     .then(handleErrors)
     .then((response) => {
       return callback(response);
